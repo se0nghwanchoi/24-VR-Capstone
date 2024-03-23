ee0aa9e2f4c84f71921d4cab47ff64e65bfbe94e
hs-1971379-KimJun <57721520+hs-1971379-kimjun@users.noreply.github.com>
2024-03-24 02:02:36 +0900

    일단 수정

---
+++ Assets/script/changescene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class changescene : MonoBehaviour
+{
+   public void ChangesceneBtn()
+    {
+        SceneManager.LoadScene("Main_Menu");
+    }
+}
