20fe74d34ac4ad402f2ca7986d781a5dd7658eff
hs-1971379-KimJun <57721520+hs-1971379-kimjun@users.noreply.github.com>
2024-04-02 00:43:15 +0900

    코드 화살표2번 수정

--- Assets/script/PhoneInteraction.cs
+++ Assets/script/PhoneInteraction.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -8,7 +7,8 @@ public class PhoneArrowAndSound : MonoBehaviour
     public XRGrabInteractable grabInteractable; // 참조할 XRGrabInteractable
     public AudioSource audioSource; // 폰 객체에 추가된 AudioSource 컴포넌트
     public AudioClip pickupSound; // 재생하고 싶은 오디오 클립
-    public GameObject arrow; // 화살표 GameObject
+    public GameObject arrow; // 첫 번째 화살표 GameObject
+    public GameObject secondArrow; // 두 번째 화살표 GameObject
 
     void OnEnable()
     {
@@ -23,13 +23,20 @@ public class PhoneArrowAndSound : MonoBehaviour
     private void HandleSelectEntered(SelectEnterEventArgs arg)
     {
         audioSource.PlayOneShot(pickupSound); // 오디오 클립 재생
-        arrow.SetActive(true); // 화살표 활성화
-        StartCoroutine(HideArrowAfterTime(5)); // 5초 후 화살표 비활성화
+        arrow.SetActive(true); // 첫 번째 화살표 활성화
+        StartCoroutine(HideArrowAfterTime(5)); // 5초 후 첫 번째 화살표 비활성화
+        StartCoroutine(HideSecondArrowAfterTime(20)); // 10초 후 두 번째 화살표 비활성화
     }
 
     private IEnumerator HideArrowAfterTime(float time)
     {
         yield return new WaitForSeconds(time);
-        arrow.SetActive(false); // 화살표 비활성화
+        arrow.SetActive(false); // 첫 번째 화살표 비활성화
+    }
+
+    private IEnumerator HideSecondArrowAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        secondArrow.SetActive(false); // 두 번째 화살표 비활성화
     }
 }
