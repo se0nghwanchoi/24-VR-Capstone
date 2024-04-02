349c2ebe6901374220e5722d2ccc75f3857343d7
topblame <113543762+topblame@users.noreply.github.com>
2024-04-03 04:49:06 +0900

    재난설정 스크립트 작성(미완)

---
+++ Assets/script/VRserver/DisasterSelect.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DisasterSelect : MonoBehaviour
+{
+    private string url = "http://localhost/Capstone24/recordSelection.php";
+
+    // 버튼에 연결할 메소드
+    public void OnDisasterSelected(string disasterName)
+    {
+        StartCoroutine(SendDisasterSelectionToServer(disasterName));
+    }
+
+    // 서버로 사용자 선택을 보내는 코루틴
+    IEnumerator SendDisasterSelectionToServer(string disaster)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("disaster", disaster);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Error: " + www.error);
+            }
+            else
+            {
+                Debug.Log("Selection recorded: " + disaster);
+            }
+        }
+    }
+}
