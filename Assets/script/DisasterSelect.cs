31a5bbe8e56801405137427352c71e2c069f0ed8
HwangGwihwan <144326170+HwangGwihwan@users.noreply.github.com>
2024-04-03 19:29:18 +0900

    database 재난 선택 연동
    database 재난 선택 연동

--- Assets/script/DisasterSelect.cs
+++ Assets/script/DisasterSelect.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class DisasterSelect : MonoBehaviour
 {
-    private string url = "http://localhost/Capstone24/recordSelection.php";
-
+    // PHP 스크립트 URL
+    private string fireurl = "http://localhost/Capstone24/fireSelection.php";
+    private string earthquakeurl = "http://localhost/Capstone24/earthquakeSelection.php";
+    private string typhoonurl = "http://localhost/Capstone24/typhoonSelection.php";
     // 버튼에 연결할 메소드
     public void OnDisasterSelected(string disasterName)
     {
@@ -16,21 +20,62 @@ public class DisasterSelect : MonoBehaviour
     // 서버로 사용자 선택을 보내는 코루틴
     IEnumerator SendDisasterSelectionToServer(string disaster)
     {
-        WWWForm form = new WWWForm();
-        form.AddField("disaster", disaster);
-        string postURL = url + "?" + form;
+        if (disaster == "fire") {
+            WWWForm form = new WWWForm();
+            form.AddField("disaster", disaster);
 
-        using (UnityWebRequest www = UnityWebRequest.Post(postURL, form))
-        {
-            yield return www.SendWebRequest();
-
-            if (www.result != UnityWebRequest.Result.Success)
+            using (UnityWebRequest www = UnityWebRequest.Post(fireurl, form))
             {
-                Debug.LogError("Error: " + www.error);
+                yield return www.SendWebRequest();
+
+                if (www.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError("Error: " + www.error);
+                }
+                else
+                {
+                    Debug.Log("Selection recorded: " + disaster);
+                }
             }
-            else
+        }
+
+        else if (disaster == "earthquake")
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("disaster", disaster);
+
+            using (UnityWebRequest www = UnityWebRequest.Post(earthquakeurl, form))
             {
-                Debug.Log("Selection recorded: " + disaster);
+                yield return www.SendWebRequest();
+
+                if (www.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError("Error: " + www.error);
+                }
+                else
+                {
+                    Debug.Log("Selection recorded: " + disaster);
+                }
+            }
+        }
+
+        else if (disaster == "typhoon")
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("disaster", disaster);
+
+            using (UnityWebRequest www = UnityWebRequest.Post(typhoonurl, form))
+            {
+                yield return www.SendWebRequest();
+
+                if (www.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError("Error: " + www.error);
+                }
+                else
+                {
+                    Debug.Log("Selection recorded: " + disaster);
+                }
             }
         }
     }
