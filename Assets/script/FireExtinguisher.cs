ba59818e086d09b7024a6dff3085d9bb6a30fa2f
LeeEunWoo <a01036459946@gmail.com>
2024-04-03 15:07:23 +0900

    update

---
+++ Assets/script/FireExtinguisher.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FireExtinguisher : MonoBehaviour
+{
+
+    public XRGrabInteractable grabInteractable; // ÂüÁ¶ÇÒ XRGrabInteractable
+    public Animator animFire;
+    public Transform particle;
+    public Transform createPoint;
+      
+          
+    void OnEnable()
+    {
+        grabInteractable.selectEntered.AddListener(HandleSelectEntered);
+    }
+
+    void OnDisable()
+    {
+        grabInteractable.selectEntered.RemoveListener(HandleSelectEntered);
+    }
+
+    private void HandleSelectEntered(SelectEnterEventArgs arg)
+    {
+        animFire.SetBool("Push_Btn", true);
+        Instantiate(particle, createPoint.position, createPoint.rotation);
+        
+    }
+
+
+
+
+}
