29752b856ac745faac5777b6ae0f8ec4683ca4ba
LeeEunWoo <a01036459946@gmail.com>
2024-04-03 14:24:20 +0900

    update

--- Assets/script/PhoneInteraction.cs
+++ Assets/script/PhoneInteraction.cs
@@ -25,7 +25,7 @@ public class PhoneArrowAndSound : MonoBehaviour
         audioSource.PlayOneShot(pickupSound); // 오디오 클립 재생
         arrow.SetActive(true); // 첫 번째 화살표 활성화
         StartCoroutine(HideArrowAfterTime(5)); // 5초 후 첫 번째 화살표 비활성화
-        StartCoroutine(HideSecondArrowAfterTime(20)); // 10초 후 두 번째 화살표 비활성화
+        StartCoroutine(HideSecondArrowAfterTime(25)); // 20초 후 두 번째 화살표 비활성화
     }
 
     private IEnumerator HideArrowAfterTime(float time)
