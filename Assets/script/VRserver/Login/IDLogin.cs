20c58f65a202840eb2a640b8103252a2b2c4a7cd
HwangGwihwan <144326170+HwangGwihwan@users.noreply.github.com>
2024-04-05 20:28:11 +0900

    user 테이블 레코드 삽입
    user 테이블 레코드 삽입

--- Assets/script/VRserver/Login/IDLogin.cs
+++ Assets/script/VRserver/Login/IDLogin.cs
@@ -1,15 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class IDLogin : MonoBehaviour
 {
     public InputField studentID_Field;
+    public string url = "http://localhost/Capstone24/CreateID.php";
 
     public void ServerLogin()
     {
         string studentID = studentID_Field.text;
         IDmanager.Instance.StudentID = studentID;
+
+        StartCoroutine(CreateID(studentID));
+    }
+
+    IEnumerator CreateID(string studentID)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("ID", studentID);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Error: " + www.error);
+            }
+            else
+            {
+                Debug.Log("ID record: " + studentID);
+            }
+        }
     }
 }
