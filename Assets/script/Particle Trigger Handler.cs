ddceff257756d5bb6cb914fec65ea8446ef33c89
LeeEunWoo <a01036459946@gmail.com>
2024-04-09 01:48:16 +0900

    add second fire

--- Assets/script/Particle Trigger Handler.cs
+++ Assets/script/Particle Trigger Handler.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class ParticleTriggerHandler : MonoBehaviour
 {
-    public GameObject Explosion;  // Explosion prefab
+    public GameObject Explosion;   // Explosion prefab
     public Transform Firepos1;    // Explosion prefab이 생성될 transform
-    public GameObject Fire;       // Fire GameObject
+    public GameObject SecondFire; // SecondFire GameObject
 
     private bool hasExploded = false;
 
@@ -13,15 +13,17 @@ public class ParticleTriggerHandler : MonoBehaviour
         if (!hasExploded)
         {
             //Debug.Log("파티클 충돌"); // 충돌 확인용 
-            Invoke("ExplodeAndDestroy", 1.5f); // 2초 후에 폭발 생성 및 제거 예약
+            Invoke("ExplodeAndActivateSecondFire", 1.5f); // 1.5초 후에 폭발 생성 및 SecondFire 활성화 예약
 
             hasExploded = true; // 폭발한 후에는 다시 폭발하지 않도록 설정
         }
     }
 
-    void ExplodeAndDestroy()
+    void ExplodeAndActivateSecondFire()
     {
         GameObject explosionInstance = Instantiate(Explosion, Firepos1.position, Firepos1.rotation); //폭발 생성
-        Destroy(explosionInstance, 1.5f); // 2초 후에 폭발 제거
+        Destroy(explosionInstance, 1.5f); // 1.5초 후에 폭발 제거
+
+        SecondFire.SetActive(true); // SecondFire 활성화
     }
 }
