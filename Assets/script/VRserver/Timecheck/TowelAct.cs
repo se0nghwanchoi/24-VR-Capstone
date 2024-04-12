6fd9166691dbbf405fee68856a6dcf8195a0171d
topblame <113543762+topblame@users.noreply.github.com>
2024-04-12 17:26:10 +0900

    towelacts

--- Assets/script/VRserver/Timecheck/TowelAct.cs
+++ Assets/script/VRserver/Timecheck/TowelAct.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class TowelAct : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class TowelAct : MonoBehaviour
         Interac_Time = GetComponent<InteractionTimer>();
         if (Interac_Time == null)
         {
-            Debug.LogError("InteractionTimer component is missing on this object!");
+            Debug.LogError("상호작용 오류 ! ");
         }
     }
 
@@ -30,6 +31,29 @@ public class TowelAct : MonoBehaviour
             Interac_Time.EndInteraction();
             float totalInteractionTime = Interac_Time.GetTotalInteractionTime();
             Debug.Log("Total Interaction Time with Handkerchief: " + totalInteractionTime);
+            StartCoroutine(Towelinteracts(totalInteractionTime));
+        }
+    }
+    private IEnumerator Towelinteracts(float interactionTime)
+    {
+        int recordID = PlayerPrefs.GetInt("RecordID"); // PlayerPrefs에서 recordID를 가져옴
+        WWWForm form = new WWWForm();
+        form.AddField("recordID", recordID);
+        form.AddField("doCode", 1);
+        form.AddField("interactionTime", interactionTime.ToString());
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/Capstone24/TowelActs.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Error while sending interaction time: " + www.error);
+            }
+            else
+            {
+                Debug.Log("Interaction time sent successfully");
+            }
         }
     }
 }
