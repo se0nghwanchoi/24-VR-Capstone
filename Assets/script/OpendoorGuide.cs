273b3198179e5c9d755da0e1a6c9b7bdd195c6a6
hs-1971379-KimJun <57721520+hs-1971379-kimjun@users.noreply.github.com>
2024-04-12 14:15:26 +0900

    문 스크립트 및 화살표 크기 조정
    수정 완

--- Assets/script/OpendoorGuide.cs
+++ Assets/script/OpendoorGuide.cs
@@ -12,6 +12,7 @@ public class OpendoorGuide : MonoBehaviour
     public GameObject arrow; // 화살표 객체
     public AudioClip broadcast; // 안내 방송 오디오 클립
     private AudioSource audioSource; // 오디오 소스 컴포넌트
+    private bool guidePlayed = false; // 가이드가 재생되었는지 확인하는 변수 추가
 
     void Start()
     {
@@ -45,10 +46,21 @@ public class OpendoorGuide : MonoBehaviour
     {
         openandclose.Play("Opening");
         open = true;
-        audioSource.clip = broadcast;
-        audioSource.Play();
-        yield return new WaitForSeconds(broadcast.length); // 오디오 재생이 끝날 때까지 기다림
-        ShowArrow();
+
+        // 처음 문이 열릴 때만 안내방송과 화살표를 활성화합니다.
+        if (!guidePlayed)
+        {
+            audioSource.clip = broadcast;
+            audioSource.Play();
+            yield return new WaitForSeconds(broadcast.length); // 오디오 재생이 끝날 때까지 기다림
+            ShowArrow();
+            guidePlayed = true; // 가이드 재생됐음을 표시
+        }
+        else
+        {
+            // 안내방송과 화살표를 보여주지 않습니다.
+            yield return null;
+        }
     }
 
     IEnumerator closing()
