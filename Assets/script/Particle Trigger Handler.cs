d660383e78ac10a4dd62bb538290a57b8ad1ccca
LeeEunWoo <a01036459946@gmail.com>
2024-04-12 15:41:59 +0900

    update

--- Assets/script/Particle Trigger Handler.cs
+++ Assets/script/Particle Trigger Handler.cs
@@ -5,16 +5,23 @@ public class ParticleTriggerHandler : MonoBehaviour
     public GameObject Explosion;   // Explosion prefab
     public Transform Firepos1;    // Explosion prefab이 생성될 transform
     public GameObject SecondFire; // SecondFire GameObject
+    public GameObject ThirdStep; // 3번째 스텝 화살표 
+    public AudioClip broadcast; // 안내 방송 오디오 클립
 
     private bool hasExploded = false;
+    private AudioSource audioSource; // 오디오 소스 컴포넌트
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); // AudioSource 컴포넌트를 가져옴
+    }
 
     void OnParticleTrigger()
     {
         if (!hasExploded)
         {
-            //Debug.Log("파티클 충돌"); // 충돌 확인용 
             Invoke("ExplodeAndActivateSecondFire", 1.5f); // 1.5초 후에 폭발 생성 및 SecondFire 활성화 예약
-
+            PlayAudioAndActivateThirdStep();
             hasExploded = true; // 폭발한 후에는 다시 폭발하지 않도록 설정
         }
     }
@@ -25,5 +32,18 @@ public class ParticleTriggerHandler : MonoBehaviour
         Destroy(explosionInstance, 1.5f); // 1.5초 후에 폭발 제거
 
         SecondFire.SetActive(true); // SecondFire 활성화
+        ThirdStep.SetActive(true); //ThirdStep 활성화
+    }
+
+    void PlayAudioAndActivateThirdStep()
+    {
+        audioSource.clip = broadcast;
+        audioSource.Play();
+        Invoke("DeactivateThirdStep", 20); // 20초 후에 ThirdStep 비활성화
+    }
+
+    void DeactivateThirdStep()
+    {
+        ThirdStep.SetActive(false); // ThirdStep 비활성화
     }
 }
