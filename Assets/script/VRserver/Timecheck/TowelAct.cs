09bd0b35f4c1084df9c9edd340f65630f321f475
topblame <113543762+topblame@users.noreply.github.com>
2024-04-13 20:03:00 +0900

    boolean check

--- Assets/script/VRserver/Timecheck/TowelAct.cs
+++ Assets/script/VRserver/Timecheck/TowelAct.cs
@@ -8,12 +8,13 @@ public class TowelAct : MonoBehaviour
     private InteractionTimer interactionTimer;
     private XRGrabInteractable grabInteractable;
     private float totalInteractionTime = 0;
+    private bool Used = false;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) // 예를 들어, Esc 키를 누르면 종료
         {
-            StartCoroutine(TowelInteracts(totalInteractionTime));
+            StartCoroutine(TowelInteracts(totalInteractionTime, Used));
         }
     }
     private void Awake()
@@ -41,6 +42,7 @@ public class TowelAct : MonoBehaviour
     {
         Debug.Log("상호작용 시작");
         interactionTimer.StartInteraction();
+        Used = true;
     }
 
     private void EndInteraction(SelectExitEventArgs args)
@@ -51,13 +53,14 @@ public class TowelAct : MonoBehaviour
         //StartCoroutine(TowelInteracts(totalInteractionTime));
     }
 
-    private IEnumerator TowelInteracts(float interactionTime)
+    private IEnumerator TowelInteracts(float interactionTime, bool usedStatus)
     {
         int recordID = PlayerPrefs.GetInt("RecordID");
         WWWForm form = new WWWForm();
         form.AddField("recordID", recordID);
         form.AddField("doCode", 1);
         form.AddField("interactionTime", interactionTime.ToString());
+        form.AddField("useStatus", usedStatus ? "1" : "0");
 
         using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/Capstone24/TowelActs.php", form))
         {
