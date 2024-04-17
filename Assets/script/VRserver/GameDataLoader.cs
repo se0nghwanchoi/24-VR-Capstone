7569f78dadb8b0eb8fc5f44cb2f51808549287f3
HwangGwihwan <144326170+HwangGwihwan@users.noreply.github.com>
2024-04-17 20:30:52 +0900

    Revert "Merge branch 'main' into sh"
    This reverts commit 9f3f39e869ae02adc21324a5d11a09cf6d1841d8, reversing
    changes made to d38b0d20651ab15d7eabc6ef3367993290707123.

--- Assets/script/VRserver/GameDataLoader.cs
+++ Assets/script/VRserver/GameDataLoader.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI; // If you want to display the results on UI elements
 
 public class GameDataLoader : MonoBehaviour
 {
+    public string apiUrl = "http://localhost/Capstone24/ApiLoad.php";
+    int recordID = PlayerPrefs.GetInt("RecordID");
+    string studentID = PlayerPrefs.GetString("studentID");
+
     public Text studentIdText;
     public Text recordIdText;
     public Text disasterText;
@@ -13,14 +17,11 @@ public class GameDataLoader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int recordID = PlayerPrefs.GetInt("RecordID");
         StartCoroutine(GetGameData(recordID));
     }
 
     IEnumerator GetGameData(int recordID)
     {
-        string apiUrl = "http://localhost/Capstone24/ApiLoad.php";
-
         string url = apiUrl + "?recordID=" + recordID;
         using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
         {
@@ -33,31 +34,26 @@ public class GameDataLoader : MonoBehaviour
             }
             else
             {
-                string jsonResponse = webRequest.downloadHandler.text;
-                GameData data = JsonUtility.FromJson<GameData>(jsonResponse);
-                Debug.Log(data);
+                ProcessGameData(webRequest.downloadHandler.text);
             }
         }
     }
 
-<<<<<<< HEAD
     void ProcessGameData(string jsonData)
     {
         GameDataList dataList = JsonUtility.FromJson<GameDataList>(jsonData);
         foreach (GameData data in dataList.items)
         {
-            Debug.Log($"Record ID: {data.recordID}, Student ID: {data.User_id}, Disaster Name: {data.disaster_id}, Play Time: {data.play_time}");
+            Debug.Log($"Record ID: {data.recordID}, Student ID: {data.User_id}, Disaster Name: {data.do_name}, Play Time: {data.play_time}");
 
             // UI 업데이트
             studentIdText.text = $"학번 : {data.User_id}";
             recordIdText.text = $"레코드 번호 : {data.recordID}";
-            disasterText.text = $"선택한 재난 : {GetDisasterName(data.disaster_id)}";
+            disasterText.text = $"선택한 재난 : {GetDisasterName(data.Do_code)}";
             playTimeText.text = $"총 Play Time : {data.play_time}";
         }
     }
 
-=======
->>>>>>> DH_db
     // Disaster code to name helper
     string GetDisasterName(int code)
     {
