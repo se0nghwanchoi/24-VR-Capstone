3a05d3379b4afd3ce9669fdd817e315be462202a
hs-1971379-KimJun <57721520+hs-1971379-kimjun@users.noreply.github.com>
2024-04-17 17:05:41 +0900

    화재대피요령 ui추가
    추가

--- Assets/script/EmergencyUI.cs
+++ Assets/script/EmergencyUI.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class EmergencyUI : MonoBehaviour
 {
+    public GameObject emergencyUI;  // 활성화할 UI 오브젝트의 참조
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            emergencyUI.SetActive(true);  // UI 오브젝트 활성화
             Debug.Log("대피 UI 생성");
+            StartCoroutine(DeactivateAfterTime(8));  // 8초 후 오브젝트 비활성화
         }
+    }
+
+    private IEnumerator DeactivateAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        emergencyUI.SetActive(false);  // 오브젝트 비활성화
+        Debug.Log("대피 UI 비활성화");
     }
 }
