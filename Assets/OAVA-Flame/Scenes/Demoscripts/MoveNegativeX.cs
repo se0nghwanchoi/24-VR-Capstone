7c1ae25f8c1ade7a9253922d5ac83e6397f4f732
hs-1971379-KimJun <57721520+hs-1971379-kimjun@users.noreply.github.com>
2024-05-01 19:08:26 +0900

    불 유료 추가
    ㅇㅇ

---
+++ Assets/OAVA-Flame/Scenes/Demoscripts/MoveNegativeX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ignis
+{
+    public class MoveNegativeX : MonoBehaviour
+    {
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Translate(new Vector3(-0.2f * Time.deltaTime, 0, 0));
+        }
+    }
+}
+
