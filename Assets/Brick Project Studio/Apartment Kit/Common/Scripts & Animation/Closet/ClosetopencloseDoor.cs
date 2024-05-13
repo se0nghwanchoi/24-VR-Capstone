fe5b2c6890438583846e8b8cb14a4b5a82d80e20
seonghwan <a01036459946@gmail.com>
2024-05-13 22:52:46 +0900

    베란다 문 xr simpleinteraction 추가

--- Assets/Brick Project Studio/Apartment Kit/Common/Scripts & Animation/Closet/ClosetopencloseDoor.cs
+++ Assets/Brick Project Studio/Apartment Kit/Common/Scripts & Animation/Closet/ClosetopencloseDoor.cs
@@ -1,72 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace SojaExiles
 
 {
 	public class ClosetopencloseDoor : MonoBehaviour
 	{
-
+		public XRSimpleInteractable simpleInteractable; // 참조할 XR
 		public Animator Closetopenandclose;
 		public bool open;
-		public Transform Player;
 
 		void Start()
 		{
 			open = false;
 		}
-
-		void OnMouseOver()
+		void OnEnable()
 		{
-			{
-				if (Player)
-				{
-					float dist = Vector3.Distance(Player.position, transform.position);
-					if (dist < 15)
-					{
-						if (open == false)
-						{
-							if (Input.GetMouseButtonDown(0))
-							{
-								StartCoroutine(opening());
-							}
-						}
-						else
-						{
-							if (open == true)
-							{
-								if (Input.GetMouseButtonDown(0))
-								{
-									StartCoroutine(closing());
-								}
-							}
-
-						}
-
-					}
-				}
-
-			}
-
+			simpleInteractable.selectEntered.AddListener(HandleSelectEntered);
 		}
 
-		IEnumerator opening()
+		void OnDisable()
 		{
-			print("you are opening the door");
-			Closetopenandclose.Play("ClosetOpening");
-			open = true;
-			yield return new WaitForSeconds(.5f);
+			simpleInteractable.selectEntered.RemoveListener(HandleSelectEntered);
 		}
+        private void HandleSelectEntered(SelectEnterEventArgs arg)
+        {
+            if (open == false)
+            {
+                StartCoroutine(opening());
+            }
+            else
+            {
+                if (open == true)
+                {
+                    StartCoroutine(closing());
+                }
 
-		IEnumerator closing()
-		{
-			print("you are closing the door");
-			Closetopenandclose.Play("ClosetClosing");
-			open = false;
-			yield return new WaitForSeconds(.5f);
-		}
+            }
+        }
+
+        IEnumerator opening()
+        {
+
+            Closetopenandclose.Play("ClosetOpening");
+            open = true;
+            yield return new WaitForSeconds(.5f);
+        }
+
+        IEnumerator closing()
+        {
+
+            Closetopenandclose.Play("ClosetClosing");
+            open = false;
+            yield return new WaitForSeconds(.5f);
+        }
 
 
-	}
+    }
 }
